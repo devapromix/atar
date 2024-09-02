ffd9dca4a88d3613a8ecada706c7ee58cf8a11ed
devapromix <maxwof@ukr.net>
2024-09-02 08:57:59 +0300

    Added races.

--- sources/Trollhunter.Race.pas
+++ sources/Trollhunter.Race.pas
@@ -7,33 +7,6 @@ uses
   System.Generics.Collections,
   Trollhunter.Skill;
 
-{  const
-  // High Elf
-  NameLangID: 187; BeginDescr: 347; EndDescr: 365; Strength: - 4;
-  Dexterity: 0; Intelligence: 4; Speed: 0; RHWeapon: (ID: 'SHORTSWORD';
-  Count: 1); LHWeapon: (ID: ''; Count: 0);
-  Skills: ((Skill: skMagic; Level: 10), (Skill: skTrap; Level: 10));), (
-  // Night Elf
-  NameLangID: 188; BeginDescr: 348; EndDescr: 366; Strength: 1; Dexterity: 2;
-  Intelligence: - 2; Speed: - 1; RHWeapon: (ID: 'HUNTBOW'; Count: 1);
-  LHWeapon: (ID: 'ARROW'; Count: 75);
-  Skills: ((Skill: skBow; Level: 10), (Skill: skTrap; Level: 10));), (
-  // Dark Elf
-  NameLangID: 189; BeginDescr: 349; EndDescr: 367; Strength: - 3;
-  Dexterity: 2; Intelligence: 0; Speed: 1; RHWeapon: (ID: 'LIGHTCROSSBOW';
-  Count: 1); LHWeapon: (ID: 'BOLT'; Count: 75);
-  Skills: ((Skill: skCrossBow; Level: 10), (Skill: skTrap; Level: 10));), (
-  // Deep Dwarf
-  NameLangID: 190; BeginDescr: 350; EndDescr: 368; Strength: 1; Dexterity: 1;
-  Intelligence: 0; Speed: - 2; RHWeapon: (ID: 'HATCHET'; Count: 1);
-  LHWeapon: (ID: 'SMALLSHIELD'; Count: 1);
-  Skills: ((Skill: skAxe; Level: 10), (Skill: skShield; Level: 10));), (
-  // Cave Dwarf
-  NameLangID: 191; BeginDescr: 351; EndDescr: 369; Strength: - 1;
-  Dexterity: 4; Intelligence: - 2; Speed: - 1; RHWeapon: (ID: 'STONEHAMMER';
-  Count: 1); LHWeapon: (ID: ''; Count: 0);
-  Skills: ((Skill: skMace; Level: 15), (Skill: skTrap; Level: 5));)); }
-
 type
   TRace = class(TObject)
   private
